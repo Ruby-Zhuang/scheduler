c3b4546e80d9fd74dc4ac0c2256c6fc44e2d1199
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-10 13:41:38 -0400

    Build button component by applying CSS conditionally and adding interactivity using onClick and disabled props. Visually test component using Storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,25 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  // Conditionally apply CSS depending on props by building up className string
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}   // Adding interactivity
+      disabled={props.disabled} // Adding interactivity (when a button is disabled it should not fire click events)
+    >
+      {/* Variable button text */}
+      {props.children}
+    </button>
+  );
 }
