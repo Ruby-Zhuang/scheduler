dc04f1e1fbf125021f7f550d7fc4a6282b039cd2
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-10 17:44:12 -0400

    Update Application.js to track the currently selected day using the useState Hook.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 
@@ -24,6 +24,9 @@ const days = [
 ];
 
 export default function Application(props) {
+  // Track the currently selected day, has to be at the top level in the body of a function component
+  const [ day, setDay ] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -37,8 +40,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           < DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
