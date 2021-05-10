b868b0f47c65c4344d33c9f0c4ba72d80c179cee
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-10 15:15:38 -0400

    Update how the spots feature displays the remaining spots available.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,21 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   });
 
+  // Format spots text depending on number of spots left
+  const formatSpots = function() {
+    const spotsRemaining = props.spots;
+
+    if (spotsRemaining) {
+      return (spotsRemaining === 1 ? `1 spot remaining` : `${spotsRemaining} spots remaining`);
+    } else {
+      return `no spots remaining`;
+    }
+  }
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
