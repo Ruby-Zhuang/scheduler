221754e5141b49d28d198a43df67482c3e2bd6e2
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-10 21:14:41 -0400

    Build InterviewerListItem component by adding base HTML, styling, adding stories

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // BUTTON STORIES -------------------------------------------------------------
@@ -75,4 +77,41 @@ storiesOf("DayList", module)
   ))
   .add("Tuesday", () => (
     <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ));
+
+///////////////////////////////////////////////////////////////////////////////
+// INTERVIEWERLISTITEM STORIES ------------------------------------------------
+///////////////////////////////////////////////////////////////////////////////
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
   ));
