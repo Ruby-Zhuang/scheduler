6150c8a4eb056af158d7dc0c67340916a3fa6329
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-11 18:02:56 -0400

    Prevent default form submission and fix onSave function by passing in the curerent name and selected interviewer

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -30,14 +30,14 @@ export default function Form(props) {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
+            value={name} // Current name
+            onChange={(event) => setName(event.target.value)} // Update input with current text
             /*
               This must be a controlled component
             */
@@ -45,14 +45,14 @@ export default function Form(props) {
         </form>
         <InterviewerList 
           interviewers={interviewers} 
-          value={interviewer} 
-          onChange={setInterviewer} 
+          value={interviewer} // Current id of interviewer
+          onChange={setInterviewer} // Function to update selected interviewer
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={onSave}>Save</Button>
+          <Button confirm onClick={() => onSave(name, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
