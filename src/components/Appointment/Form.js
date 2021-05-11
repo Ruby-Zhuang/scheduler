9ef5b5d1cfd8e83766a16ff098df7f3287160873
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-11 17:46:12 -0400

    Add state to Appointment Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
-/* Form component keeps track of the current text input value and the currently selected interviewer
+/* 
+ * Form component keeps track of the current text input value and the currently selected interviewer
  * Allows user to input their information, save it and edit it
  */
 export default function Form(props) {
-  const { name, interviewers, interviewer, onSave, onCancel, setInterviewer } = props;
+  // Add state and set default values
+  const [ name, setName ] = useState(props.name || "");
+  const [ interviewer, setInterviewer ] = useState(props.interviewer || null);
+  
+  // Deconstruct remaining props
+  const {interviewers, onSave, onCancel } = props;
 
+  // Clear and reset the form values
+  const reset = () => {
+    setName('');
+    setInterviewer(null);
+  };
+
+  // Handle cancel
+  const cancel = () => {
+    reset();
+    onCancel();
+  };
+
+  // Form component
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -18,16 +37,21 @@ export default function Form(props) {
             type="text"
             placeholder="Enter Student Name"
             value={name}
+            onChange={(event) => setName(event.target.value)}
             /*
               This must be a controlled component
             */
           />
         </form>
-        <InterviewerList interviewers={interviewers} value={interviewer} onChange={setInterviewer} />
+        <InterviewerList 
+          interviewers={interviewers} 
+          value={interviewer} 
+          onChange={setInterviewer} 
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={onSave}>Save</Button>
         </section>
       </section>
