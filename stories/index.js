5eb25a1b37b1e1362cecf60472b8c73e7d82f82d
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-11 13:43:57 -0400

    Create empty Appointment component to load the styles shared between all of the child views. Add stories for Appointment

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment";
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // BUTTON STORIES -------------------------------------------------------------
@@ -144,3 +146,13 @@ storiesOf("InterviewerListItem", module)
         onChange={action("setInterviewer")}
       />
     ));
+
+///////////////////////////////////////////////////////////////////////////////
+// APPOINTMENT STORIES --------------------------------------------------------
+///////////////////////////////////////////////////////////////////////////////
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
