d3de240c69b912c0ebe2f1b301513189d92d374c
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-11 14:09:23 -0400

    Add Empty component as child view for Appointment component and add story

--- stories/index.js
+++ stories/index.js
@@ -22,6 +22,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -164,5 +165,6 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
 
