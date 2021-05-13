94c1810df93265e33ef3cf2ebbc761c62d767836
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-13 14:46:05 -0400

    Third useVisualMode test and back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition(newMode) {
     setMode(newMode);
-  };
+    setHistory((prev) => [...prev, newMode]);
+  }
 
-  return { mode, transition };
+  function back() {
+    if (history.length <= 1) return;  // Limit to not allow user to go back past the initial mode
+    
+    const newHistory = history.slice(0, -1); // Shallow copies history arrow without last item
+    const prevMode = newHistory[newHistory.length - 1];
+
+    setHistory(newHistory);
+    setMode(prevMode);
+  }
+
+  return { mode, transition, back };
 }
 
 export default useVisualMode;
