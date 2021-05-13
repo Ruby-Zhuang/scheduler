2d155e1cf26247a6f85a96a8094f6104a9471081
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-13 16:49:14 -0400

    Implement getInterviewersForDay selector and tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -33,3 +33,20 @@ export function getInterview(state, interview) {
   
   return interviewData; // { student, interviewer:{} }
 }
+
+// RETURNS AN ARRAY OF INTERVIEWERS FOR THAT DAY (EMPTY ARRAY IF NOTHING FOUND)
+export function getInterviewersForDay(state, day) {
+  const { days, interviewers } = state; // days [{}, {}, {}...] and appointments {{}, {}, {}...}
+
+  // Find the object in our state.days array who's name matches the provided day
+  const dayObjectFound = days.find((dayData) => dayData.name === day);
+
+  // Returns an empty array when: the day is not found/the days data is empty || no interviewers for the day
+  if (!dayObjectFound || dayObjectFound.interviewers.length === 0) return [];
+
+  // Iterate over interviewerIds array and for each id, return the corresponding appointment object from state.interviewers
+  const interviewerIds = dayObjectFound.interviewers;
+  const dailyInterviewers = interviewerIds.map((interviewerId) => interviewers[interviewerId]);
+
+  return dailyInterviewers;
+}
