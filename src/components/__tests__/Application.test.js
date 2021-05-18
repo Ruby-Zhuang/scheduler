15dead4378cfd0cfc80e45b4bcdbf0781736d50d
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-18 09:49:08 -0400

    Creatte __mock__ folder to mock axios get function. Update first Application test to prove that the application loads without crashing and makes the requests to the API server to retrieve appointment data to display in the schedule.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React from 'react';
 
-import { render, cleanup } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+} from '@testing-library/react';
 
-import Application from "components/Application";
+import Application from 'components/Application';
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it('defaults to Monday and changes the schedule when a new day is selected', () => {
+  const { getByText } = render(<Application />);
+
+  // waitForElement function returns a promise that resolves when the callback returns a truthy value and rejects after a time out when it cannot find the specified text
+  // When we return a Promise from the test function, the Jest framework knows that the test isn't complete until the promise chain has resolved or rejected.
+  return waitForElement(() => getByText('Monday')).then(() => {
+    fireEvent.click(getByText('Tuesday'));
+    expect(getByText('Leopold Silvers')).toBeInTheDocument();
+  });
 });
