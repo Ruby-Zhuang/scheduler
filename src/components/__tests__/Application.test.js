35adb49235161ff1b549fd4baaccc7aee3bf941d
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-18 15:26:17 -0400

    Implement integration test for editting an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -29,6 +29,7 @@ describe('Application', () => {
     });
   });
 
+  // BOOKING AN INTERVIEW
   it('loads data, books an interview and reduces the spots remaining for Monday by 1', async () => {
     // 1. Render the Application
     /* Container represents the DOM tree that we are working with, and we can pass it to any of the imported queries. */
@@ -68,6 +69,7 @@ describe('Application', () => {
     expect(getByText(day, 'no spots remaining')).toBeInTheDocument();
   });
 
+  // DELETING AN INTERVIEW
   it('loads data, cancels an interview and increases the spots remaining for Monday by 1', async () => {
     // 1. Render the Application.
     /* Container represents the DOM tree that we are working with, and we can pass it to any of the imported queries. */
@@ -104,5 +106,47 @@ describe('Application', () => {
       queryByText(day, 'Monday')
     );
     expect(getByText(day, '2 spots remaining')).toBeInTheDocument();
+  });
+
+  // EDITING AN INTERVIEW
+  it('loads data, edits an interview and keeps the spots remaining for Monday the same', async () => {
+    // 1. Render the Application
+    /* Container represents the DOM tree that we are working with, and we can pass it to any of the imported queries. */
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+
+    // 3. Click the "Edit" button on the booked appointment.
+    /* Search for all of the appointments in the container and find the one that contains "Archie Cohen" */
+    const appointments = getAllByTestId(container, 'appointment');
+    const appointment = appointments.find((appointment) =>
+      queryByText(appointment, 'Archie Cohen')
+    );
+    fireEvent.click(getByAltText(appointment, 'Edit'));
+
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: 'Lydia Miller-Jones' },
+    });
+
+    // 5. Click the first interviewer in the list (might not need this)
+    fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
+
+    // 6. Click the "Save" button on that same appointment
+    fireEvent.click(getByText(appointment, 'Save'));
+
+    // 7. Check that the element with the text "Saving" is displayed
+    expect(getByText(appointment, 'Saving')).toBeInTheDocument();
+
+    // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed
+    await waitForElement(() => getByText(appointment, 'Lydia Miller-Jones'));
+
+    // 9. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining" (spots remain unchanged)
+    /* Search for all of the days in the container and find a specific one */
+    const day = getAllByTestId(container, 'day').find((day) =>
+      queryByText(day, 'Monday')
+    );
+    expect(getByText(day, '1 spot remaining')).toBeInTheDocument();
   });
 });
