b44dfee51690dde4c363577c0201c27220ad308f
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-18 14:54:59 -0400

    Implement integration test for cancelling an interview

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -93,4 +93,7 @@ export default {
   put: jest.fn((url) => {
     return Promise.resolve({ status: 204, statusText: 'No Content' });
   }),
+  delete: jest.fn((url) => {
+    return Promise.resolve({ status: 204, statusText: 'No Content' });
+  }),
 };
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -32,16 +32,15 @@ describe('Application', () => {
   it('loads data, books an interview and reduces the spots remaining for Monday by 1', async () => {
     // 1. Render the Application
     /* Container represents the DOM tree that we are working with, and we can pass it to any of the imported queries. */
-    const { container, debug } = render(<Application />);
+    const { container } = render(<Application />);
 
     // 2. Wait until the text "Archie Cohen" is displayed
     await waitForElement(() => getByText(container, 'Archie Cohen'));
 
+    // 3. Click the "Add" button on the first empty appointment
     /* Search for all of the appointments in the container and access the first one */
     const appointments = getAllByTestId(container, 'appointment');
     const appointment = appointments[0];
-
-    // 3. Click the "Add" button on the first empty appointment
     fireEvent.click(getByAltText(appointment, 'Add'));
 
     // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
@@ -61,12 +60,49 @@ describe('Application', () => {
     // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed
     await waitForElement(() => getByText(appointment, 'Lydia Miller-Jones'));
 
+    // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining"
     /* Search for all of the days in the container and find a specific one */
     const day = getAllByTestId(container, 'day').find((day) =>
       queryByText(day, 'Monday')
     );
-
-    // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining"
     expect(getByText(day, 'no spots remaining')).toBeInTheDocument();
+  });
+
+  it('loads data, cancels an interview and increases the spots remaining for Monday by 1', async () => {
+    // 1. Render the Application.
+    /* Container represents the DOM tree that we are working with, and we can pass it to any of the imported queries. */
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+
+    // 3. Click the "Delete" button on the booked appointment.
+    /* Search for all of the appointments in the container and find the one that contains "Archie Cohen" */
+    const appointments = getAllByTestId(container, 'appointment');
+    const appointment = appointments.find((appointment) =>
+      queryByText(appointment, 'Archie Cohen')
+    );
+    fireEvent.click(getByAltText(appointment, 'Delete'));
+
+    // 4. Check that the confirmation message is shown.
+    expect(
+      getByText(appointment, 'Are you sure you want to delete?')
+    ).toBeInTheDocument();
+
+    // 5. Click the "Confirm" button on the confirmation.
+    fireEvent.click(queryByText(appointment, 'Confirm'));
+
+    // 6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, 'Deleting')).toBeInTheDocument();
+
+    // 7. Wait until the element with the "Add" button is displayed.
+    await waitForElement(() => getByAltText(appointment, 'Add'));
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    /* Search for all of the days in the container and find a specific one (could split this into 2 lines) */
+    const day = getAllByTestId(container, 'day').find((day) =>
+      queryByText(day, 'Monday')
+    );
+    expect(getByText(day, '2 spots remaining')).toBeInTheDocument();
   });
 });
