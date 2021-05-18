35c6ce2a0e760604f68384d01e486d9049d6f2fc
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-18 16:01:59 -0400

    Implement integration test for error handling on save and delete

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -15,6 +15,8 @@ import {
 
 import Application from 'components/Application';
 
+import axios from 'axios';
+
 afterEach(cleanup);
 
 describe('Application', () => {
@@ -148,5 +150,54 @@ describe('Application', () => {
       queryByText(day, 'Monday')
     );
     expect(getByText(day, '1 spot remaining')).toBeInTheDocument();
+  });
+
+  // ERROR HANDLING: ON SAVE
+  it('shows the save error when failing to save an appointment', async () => {
+    axios.put.mockRejectedValueOnce();
+
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+    const appointment = getAllByTestId(container, 'appointment')[0];
+
+    fireEvent.click(getByAltText(appointment, 'Add'));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: 'Lydia Miller-Jones' },
+    });
+    fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
+    fireEvent.click(getByText(appointment, 'Save'));
+    expect(getByText(appointment, 'Saving')).toBeInTheDocument();
+
+    await waitForElement(
+      () => getByText(appointment, 'Could not save appointment.') // Why doesn't queryByText work without the period?
+    );
+
+    fireEvent.click(getByAltText(appointment, 'Close'));
+    expect(getByText(appointment, 'Save')).toBeInTheDocument(); // Back to FORM mode
+  });
+
+  // ERROR HANDLING: ON DELETE
+  it('shows the delete error when failing to delete an existing appointment', async () => {
+    axios.delete.mockRejectedValueOnce();
+
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+    const appointment = getAllByTestId(container, 'appointment').find(
+      (appointment) => queryByText(appointment, 'Archie Cohen')
+    );
+
+    fireEvent.click(getByAltText(appointment, 'Delete'));
+    expect(
+      getByText(appointment, 'Are you sure you want to delete?')
+    ).toBeInTheDocument();
+    fireEvent.click(queryByText(appointment, 'Confirm'));
+    expect(getByText(appointment, 'Deleting')).toBeInTheDocument();
+
+    await waitForElement(() =>
+      getByText(appointment, 'Could not delete appointment.')
+    );
+
+    fireEvent.click(getByAltText(appointment, 'Close'));
+    expect(getByAltText(appointment, 'Delete')).toBeInTheDocument(); // Back to SHOW mode
   });
 });
