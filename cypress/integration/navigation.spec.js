2d60f98b1ce77c0b460b98f298cb40af241108d5
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-19 13:38:47 -0400

    Refactor navigation test to use data-* attribute to provide context to selectors and isolate them from CSS or JS changes.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,13 @@ describe('Navigation', () => {
   it('should visit root', () => {
     cy.visit('/');
   });
+
+  it('should navigate to Tuesday', () => {
+    cy.visit('/');
+
+    // Finds the list item with data-testid attribute, clicks it and checks it for the correct class for a selected item.
+    cy.contains('[data-testid=day]', 'Tuesday')
+      .click()
+      .should('have.class', 'day-list__item--selected');
+  });
 });
