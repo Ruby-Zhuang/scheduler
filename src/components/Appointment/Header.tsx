4617a2e77d5efc060d64d57f9681fdda2cddbd89
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2022-07-22 11:15:08 -0400

    TS interface for HeaderProps

--- src/components/Appointment/Header.tsx
+++ src/components/Appointment/Header.tsx
@@ -1,5 +1,9 @@
+interface HeaderProps {
+  time: string;
+}
+
 // Header component displays the time for the appointment
-export default function Header(props) {
+export default function Header(props: HeaderProps) {
   return (
     <header className="appointment__time">
       <h4 className="text--semi-bold">{props.time}</h4>
