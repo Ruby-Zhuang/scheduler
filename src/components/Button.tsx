0dce56245aed2f17d5d03a37cf3f1a2e9116462d
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2022-07-22 11:16:37 -0400

    TS interface for ButtonProps

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,15 @@
 import classNames from 'classnames';
 import 'components/Button.scss';
 
-export default function Button(props) {
+interface ButtonProps {
+  confirm?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+  onClick: () => void;
+  children: string; // QUESTION: best way to handle children prop?
+}
+
+export default function Button(props: ButtonProps) {
   // Use classnames library to help compose the list of classes for the button
   const buttonClass = classNames('button', {
     'button--confirm': props.confirm, // Add confirm class if true
